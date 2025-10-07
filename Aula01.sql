aa6ec7eca6f9e78d0010e2546193f809acfde555
Caio Comitre Rossi <49957185+caioross@users.noreply.github.com>
2025-10-06 22:15:52 -0300

    Finalizando o exercicio
    Inserindo os selects e inserts que faltaram

--- Aula01.sql
+++ Aula01.sql
@@ -16,6 +16,36 @@ USE db0610_Cadastro;
 CREATE TABLE tb_alunos(
 	id_aluno INT IDENTITY(1,1) PRIMARY KEY,
 	nome VARCHAR(50) NOT NULL,
-	idade
-	uf
+	idade VARCHAR(3),
+	uf CHAR(2) NOT NULL
 	);
+
+-- Inserir alguns dados na tabela
+INSERT INTO tb_alunos (nome, idade, uf)
+VALUES ('Caio',38,'SP');
+
+SELECT * FROM tb_alunos;
+
+INSERT INTO tb_alunos (nome, idade, uf)
+VALUES ('Diego',18,'AC');
+
+-- Adicionando uma coluna de cidade
+ALTER TABLE tb_alunos ADD cidade VARCHAR(30);
+
+ALTER TABLE tb_alunos DROP COLUMN cidade
+
+--selecionar apenas nome e idade
+SELECT nome, idade FROM tb_alunos;
+
+-- Mostra apenas pessoas de SP
+SELECT * FROM tb_alunos WHERE uf = 'SP';
+
+-- mostra apenas pessoas com idade maior que 30
+SELECT * FROM tb_alunos WHERE idade >= 30;
+
+/*
+>= maior ou igual
+<= menor ou igual
+< maior
+> menor
+*/

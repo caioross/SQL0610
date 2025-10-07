0ed99eb09e53c0edb687497c8475a00c7f0a8768
Caio Comitre Rossi <49957185+caioross@users.noreply.github.com>
2025-10-06 21:40:05 -0300

    Primeiro Upload
    Esse foi meu primeiro commit no github

---
+++ Aula01.sql
@@ -0,0 +1,21 @@
+-- Criar seu primeiro comentario
+
+
+/*
+é um comentario
+de varias
+linhas
+*/
+
+CREATE DATABASE db0610_Cadastro;
+
+USE db0610_Cadastro;
+
+-- Criando uma tabela de pessoas!
+
+CREATE TABLE tb_alunos(
+	id_aluno INT IDENTITY(1,1) PRIMARY KEY,
+	nome VARCHAR(50) NOT NULL,
+	idade
+	uf
+	);
